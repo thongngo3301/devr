af268e9d3c8987d570d97531695765ae69194cdf
thongnd <thongnd@gmail.com>
2023-02-28 09:21:06 +0700

    update playbook

--- setup/init.sh
+++ setup/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update -y
 sudo apt install ansible -y
 ansible-playbook -i inventory.ini playbook.yml
